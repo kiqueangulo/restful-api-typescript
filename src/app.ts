f6b707690557c3310816410a98f15a753ed6116c
kiqueangulo <kiqueangulo@gmail.com>
2022-09-20 02:41:04 -0400

    Set up and check routes file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 const app = express();
 
@@ -10,4 +11,6 @@ app.listen(port, async () => {
   logger.info(`App running at http://localhost:${port}`);
 
   await connect();
+
+  routes(app);
 });

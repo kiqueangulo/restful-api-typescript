d142c53647d8eb12ec9943381dacf523a4f1bc2d
kiqueangulo <kiqueangulo@gmail.com>
2022-09-20 02:55:58 -0400

    Create the user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;

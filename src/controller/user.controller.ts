5eb3d5d4bd66f2edab39b99975e26a424f4882bf
kiqueangulo <kiqueangulo@gmail.com>
2022-09-21 15:11:49 -0400

    Create user schema for validation

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,6 +6,7 @@ import logger from "../utils/logger";
 export async function createUserHandler(req: Request, res: Response) {
   try {
     const user = await createUser(req.body);
+    return user;
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Express, Request, Response } from "express";
 
+import { createUserHandler } from "./controller/user.controller";
+
 function routes(app: Express) {
   app.get("/healthCheck", async (req: Request, res: Response) =>
     res.sendStatus(200)
   );
+
+  app.post("/api/users", createUserHandler);
 }
 
 export default routes;

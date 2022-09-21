ea48ad1e9d66b6283b13ed02fbd3a495c0e7c46c
kiqueangulo <kiqueangulo@gmail.com>
2022-09-21 16:43:04 -0400

    Do not send the password when returning the user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { omit } from "lodash";
 
 import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../service/user.service";
@@ -10,7 +11,7 @@ export async function createUserHandler(
 ) {
   try {
     const user = await createUser(req.body);
-    return res.send(user);
+    return res.send(omit(user.toJSON(), "password"));
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);

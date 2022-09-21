d6af96b3c0f1244a2ec210f66406c578df133e97
kiqueangulo <kiqueangulo@gmail.com>
2022-09-21 14:54:59 -0400

    Import createUser

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+
+import { createUser } from "../service/user.service";
 import logger from "../utils/logger";
 
-export function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(req: Request, res: Response) {
   try {
-    // const user = await // create user service
+    const user = await createUser(req.body);
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);

247177f5dd19465bfc4f425769290fbc5fee5843
kiqueangulo <kiqueangulo@gmail.com>
2022-09-21 14:48:10 -0400

    Create user handler

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import logger from "../utils/logger";
+
+export function createUserHandler(req: Request, res: Response) {
+  try {
+    // const user = await // create user service
+  } catch (e: any) {
+    logger.error(e);
+    return res.status(409).send(e.message);
+  }
+}

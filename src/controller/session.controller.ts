0bcd6e869165c573da918701c79ad46cad1912ea
kiqueangulo <kiqueangulo@gmail.com>
2022-09-21 17:00:58 -0400

    Plan what is needed to create the session controller

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+  // Validate the user password
+  // Create a session
+  // Create an access token
+  // Create a refresh token
+  // Return access and refresh tokens
+}

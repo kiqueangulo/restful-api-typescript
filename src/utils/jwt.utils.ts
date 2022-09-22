3b9f8e9d73fa136d592c4879c7d0c6bc4cc85833
kiqueangulo <kiqueangulo@gmail.com>
2022-09-22 02:20:02 -0400

    Set up signJwt function

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -4,6 +4,11 @@ import config from "config";
 const privateKey = config.get<string>("privateKey");
 const publicKey = config.get<string>("publicKey");
 
-function signJwt() {}
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, privateKey, {
+    ...(options && options),
+    algorithm: "RS256",
+  });
+}
 
 function verifyJwt() {}

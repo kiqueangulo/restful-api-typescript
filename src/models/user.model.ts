ffed08c899440e2529738ccc653b3b3f513dc6a2
kiqueangulo <kiqueangulo@gmail.com>
2022-09-22 01:57:53 -0400

    Create service to validate passwords

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -52,6 +52,6 @@ userSchema.methods.comparePassword = async function (
   return bcrypt.compare(candidatePassword, user.password).catch((err) => false);
 };
 
-const UserModel = mongoose.model("User", userSchema);
+const UserModel = mongoose.model<UserDocument>("User", userSchema);
 
 export default UserModel;

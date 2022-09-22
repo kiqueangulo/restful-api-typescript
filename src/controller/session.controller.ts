437b8a6b3babc2ea4c90d23d174f0ce009a326d5
kiqueangulo <kiqueangulo@gmail.com>
2022-09-22 02:07:05 -0400

    Validate user and create session

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
 
+import { validatePassword } from "../service/user.service";
+import { createSession } from "../service/session.service";
+
 export async function createUserSessionHandler(req: Request, res: Response) {
   // Validate the user password
+  const user = await validatePassword(req.body);
+
+  if (!user) return res.status(401).send("Invalid email or password");
+
   // Create a session
+  const session = createSession(user._id, req.get("userAgent") || "");
+
   // Create an access token
   // Create a refresh token
   // Return access and refresh tokens

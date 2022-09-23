6b769d5e403a3a24910640a490a1da3a724b5605
kiqueangulo <kiqueangulo@gmail.com>
2022-09-23 13:36:31 -0400

    Create controller to disbale access when calling delete session

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -2,7 +2,11 @@ import { Request, Response } from "express";
 import config from "config";
 
 import { validatePassword } from "../service/user.service";
-import { createSession, findSessions } from "../service/session.service";
+import {
+  createSession,
+  findSessions,
+  updateSession,
+} from "../service/session.service";
 import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
@@ -36,4 +40,12 @@ export async function getUserSessionsHandler(req: Request, res: Response) {
   const sessions = await findSessions({ user: userId, valid: true });
 
   return res.send(sessions);
+}
+
+export async function deleteSessionHandler(req: Request, res: Response) {
+  const sessionId = res.locals.user.session;
+
+  await updateSession({ _id: sessionId }, { valid: false });
+
+  return res.send({ accessToken: null, refreshToken: null });
 }

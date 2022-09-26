d7ddef8fbf4c969ffec3b1b4e33676d90b879565
kiqueangulo <kiqueangulo@gmail.com>
2022-09-26 18:03:00 -0400

    Add env
    Still trying to make env variable to work

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -11,7 +11,7 @@ export async function createUserHandler(
 ) {
   try {
     const user = await createUser(req.body);
-    return res.send(omit(user.toJSON(), "password"));
+    return res.send(omit(user, "password"));
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);

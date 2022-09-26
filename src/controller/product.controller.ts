e372aba26322a0a79aee1ebfc0ae6112e9c53714
kiqueangulo <kiqueangulo@gmail.com>
2022-09-26 13:32:38 -0400

    Set up deleteProductHandler

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -6,6 +6,7 @@ import {
 } from "../schema/product.schema";
 import {
   createProduct,
+  deleteProduct,
   findAndUpdateProduct,
   findProduct,
 } from "../service/product.service";
@@ -42,12 +43,25 @@ export async function updateProductHandler(
   return res.send(updatedProduct);
 }
 
-export async function getProductHandler(
+export async function deleteProductHandler(
   req: Request<UpdateProductInput["params"]>,
   res: Response
-) {}
+) {
+  const userId = res.locals.user._id;
+  const productId = req.params.productId;
 
-export async function deleteProductHandler(
+  const product = await findProduct({ productId });
+
+  if (!product) return res.sendStatus(404);
+
+  if (product.user !== userId) return res.sendStatus(403);
+
+  await deleteProduct({ productId });
+
+  return res.sendStatus(200);
+}
+
+export async function getProductHandler(
   req: Request<UpdateProductInput["params"]>,
   res: Response
 ) {}

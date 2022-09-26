3410d83134efbdf202606099d69198d8752b0398
kiqueangulo <kiqueangulo@gmail.com>
2022-09-26 15:01:12 -0400

    Create routes for product and downgrade nanoid version
    The latest version is made for ESM and it is not supported yet

--- src/routes.ts
+++ src/routes.ts
@@ -6,8 +6,20 @@ import {
   getUserSessionsHandler,
 } from "./controller/session.controller";
 import { createUserHandler } from "./controller/user.controller";
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "./controller/product.controller";
 import { createUserSchema } from "./schema/user.schema";
 import { createSessionSchema } from "./schema/session.schema";
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from "./schema/product.schema";
 import validateResource from "./middleware/validateResource";
 import requireUser from "./middleware/requireUser";
 
@@ -27,6 +39,30 @@ function routes(app: Express) {
   app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+  app.post(
+    "/api/products",
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+
+  app.put(
+    "/api/products",
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.get(
+    "/api/products",
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+
+  app.delete(
+    "/api/products",
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 
 export default routes;

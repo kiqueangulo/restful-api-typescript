64d032890e8ce88e927b6efd6f98c6c4b186f39c
kiqueangulo <kiqueangulo@gmail.com>
2022-09-26 12:49:36 -0400

    Establish the structure for product controllers

---
+++ src/controller/product.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export async function createProductHandler(req: Request, res: Response) {}
+
+export async function updateProductHandler(req: Request, res: Response) {}
+
+export async function getProductHandler(req: Request, res: Response) {}
+
+export async function deleteProductHandler(req: Request, res: Response) {}

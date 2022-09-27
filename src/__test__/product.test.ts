88c063363e0b24b58b858b340ff208873bfbe409
kiqueangulo <kiqueangulo@gmail.com>
2022-09-27 03:06:18 -0400

    Fix error and pass test

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,10 +1,23 @@
 import supertest from "supertest";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 import createServer from "../utils/server";
 
 const app = createServer();
 
 describe("product", () => {
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+
+    await mongoose.connect(mongoServer.getUri());
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  });
+
   describe("get product route", () => {
     describe("given the product does not exist", () => {
       it("should return a 404", async () => {

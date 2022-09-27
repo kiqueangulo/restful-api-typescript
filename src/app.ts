5590ae911053945ff49694f6d689e2aac1ed8437
kiqueangulo <kiqueangulo@gmail.com>
2022-09-27 02:39:08 -0400

    Refactor app initiation

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,14 @@
-import express from "express";
 import config from "config";
 
+import createServer from "./utils/server";
 import connectDB from "./utils/connectDB";
 import logger from "./utils/logger";
-import routes from "./routes";
-import deserializeUser from "./middleware/deserializeUser";
 
-export const app = express();
-
-app.use(express.json());
-app.use(deserializeUser);
+const app = createServer();
 
 const port = config.get<number>("port");
 app.listen(port, async () => {
   logger.info(`App running at http://localhost:${port}`);
 
   await connectDB();
-
-  routes(app);
 });

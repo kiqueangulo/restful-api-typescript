1d94831295ef9f487dfe518dc6f89bdc06767b31
kiqueangulo <kiqueangulo@gmail.com>
2022-09-28 17:12:12 -0400

    Establish what tests I want to run

---
+++ src/__test__/user.test.ts
@@ -0,0 +1,9 @@
+describe("user", () => {
+  // User registration
+  //      The username and password get validation
+  //      Verify that passwords must match (password and confirm password)
+  //      Verify that the handler handles any errors
+  //
+  // Creating a user session
+  //      A user can login with a valid email and password
+});

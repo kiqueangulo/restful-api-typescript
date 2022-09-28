7b6f7c80fd010e2ca96b683eba5c91412c0b09a8
kiqueangulo <kiqueangulo@gmail.com>
2022-09-28 17:48:01 -0400

    Validate username and password test completed

--- src/__test__/user.test.ts
+++ src/__test__/user.test.ts
@@ -1,3 +1,11 @@
+import mongoose from "mongoose";
+import supertest from "supertest";
+
+import createServer from "../utils/server";
+import * as UserService from "../service/user.service";
+
+const app = createServer();
+
 const userInput = {
   email: "test@example.com",
   name: "Jane Doe",
@@ -5,12 +13,35 @@ const userInput = {
   passwordConfirmation: "Password123",
 };
 
+const userId = new mongoose.Types.ObjectId().toString();
+
+const userPayload = {
+  _id: userId,
+  email: "jane.doe@example.com",
+  name: "Jane Doe",
+};
+
 describe("user", () => {
   // User registration
   describe("user registration", () => {
     // The username and password get validation
     describe("given the username and password are valid", () => {
-      it("should retrun the user payload", () => {});
+      it("should retrun the user payload", async () => {
+        const createUserServiceMock = jest
+          .spyOn(UserService, "createUser")
+          // @ts-ignore
+          .mockReturnValueOnce(userPayload);
+
+        const { statusCode, body } = await supertest(app)
+          .post("/api/users")
+          .send(userInput);
+
+        expect(statusCode).toBe(200);
+
+        expect(body).toEqual(userPayload);
+
+        expect(createUserServiceMock).toHaveBeenCalledWith(userInput);
+      });
     });
 
     // Verify that passwords must match (password and confirm password)

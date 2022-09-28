48b6267075a210dda5fed45de5ac85912320b414
kiqueangulo <kiqueangulo@gmail.com>
2022-09-28 16:59:40 -0400

    Complete product tests

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -4,6 +4,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 
 import createServer from "../utils/server";
 import { createProduct } from "../service/product.service";
+import { signJwt } from "../utils/jwt.utils";
 
 const app = createServer();
 
@@ -16,6 +17,12 @@ export const productPayload = {
     "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
   price: 879.99,
   image: "https://i.imgur.com/QlRphfQ.jpg",
+};
+
+export const userPayload = {
+  _id: userId,
+  email: "jane.doe@example.com",
+  name: "Jane Doe",
 };
 
 describe("product", () => {
@@ -49,6 +56,43 @@ describe("product", () => {
 
         expect(statusCode).toBe(200);
         expect(body.productId).toBe(product.productId);
+      });
+    });
+  });
+
+  describe("create product route", () => {
+    describe("given a user is not logged in", () => {
+      it("should return a 403", async () => {
+        const { statusCode } = await supertest(app).post("/api/products");
+
+        expect(statusCode).toBe(403);
+      });
+    });
+
+    describe("given a user is logged in", () => {
+      it("should return a 200 and create the product", async () => {
+        const jwt = signJwt(userPayload);
+
+        const { statusCode, body } = await supertest(app)
+          .post("/api/products")
+          .set("Authorization", `Bearer ${jwt}`)
+          .send(productPayload);
+
+        expect(statusCode).toBe(200);
+
+        expect(body).toEqual({
+          __v: 0,
+          _id: expect.any(String), // These fields need to expect any string because they change with every call
+          createdAt: expect.any(String),
+          description:
+            "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
+          image: "https://i.imgur.com/QlRphfQ.jpg",
+          price: 879.99,
+          productId: expect.any(String),
+          title: "Canon EOS 1500D DSLR Camera with 18-55mm Lens",
+          updatedAt: expect.any(String),
+          user: expect.any(String),
+        });
       });
     });
   });

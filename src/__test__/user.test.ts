c9fdb59ff1d422511eb5ae4ce8b9e42235c7dccd
kiqueangulo <kiqueangulo@gmail.com>
2022-09-28 17:27:59 -0400

    Get the structure of tests ready to code

--- src/__test__/user.test.ts
+++ src/__test__/user.test.ts
@@ -1,22 +1,27 @@
 describe("user", () => {
   // User registration
   describe("user registration", () => {
+    // The username and password get validation
     describe("given the username and password are valid", () => {
       it("should retrun the user payload", () => {});
     });
 
+    // Verify that passwords must match (password and confirm password)
     describe("given the passwords do not match", () => {
       it("should return a 400", () => {});
     });
 
+    // Verify that the handler handles any errors
     describe("given the user sevice throws", () => {
       it("should return a 409 error", () => {});
     });
   });
-  //      The username and password get validation
-  //      Verify that passwords must match (password and confirm password)
-  //      Verify that the handler handles any errors
-  //
+
   // Creating a user session
-  //      A user can login with a valid email and password
+  describe("create user session", () => {
+    // A user can login with a valid email and password
+    describe("given the username and password are valid", () => {
+      it("should return a signed access token and refresh token", () => {});
+    });
+  });
 });

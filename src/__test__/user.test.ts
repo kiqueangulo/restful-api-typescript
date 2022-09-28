90e0558211297b79ed5d9744d79b789ff6686942
kiqueangulo <kiqueangulo@gmail.com>
2022-09-28 17:23:14 -0400

    Describe the tests to do for registration

--- src/__test__/user.test.ts
+++ src/__test__/user.test.ts
@@ -1,5 +1,18 @@
 describe("user", () => {
   // User registration
+  describe("user registration", () => {
+    describe("given the username and password are valid", () => {
+      it("should retrun the user payload", () => {});
+    });
+
+    describe("given the passwords do not match", () => {
+      it("should return a 400", () => {});
+    });
+
+    describe("given the user sevice throws", () => {
+      it("should return a 409 error", () => {});
+    });
+  });
   //      The username and password get validation
   //      Verify that passwords must match (password and confirm password)
   //      Verify that the handler handles any errors
